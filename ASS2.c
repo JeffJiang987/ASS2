d1445b893ca50f3efd2c16d09d93d91c5f9c2ebb
Jeffry <jeffsbigprecious@Jeffrys-MacBook-Air.local>
2026-05-11 14:18:35 +1000

    initial commit

---
+++ ASS2.c
@@ -0,0 +1,100 @@
+/* Program to plan package deliveries to be done by drone
+
+   Skeleton written by Alistair Moffat, ammoffat@unimelb.edu.au,
+   May 2026, with the intention that it be modified by students
+   to add functionality, as required by the assignment specification.
+
+   Student Authorship Declaration:
+
+   (1) I certify that except for the code provided in the initial
+   skeleton file the program contained in this submission is my
+   own work, except where explicitly noted below in connection with
+   the use of AI.  I understand that work that has been developed
+   by another student, or by me in collaboration with other students,
+   or by non-students as a result of request, solicitation, or
+   payment, may not be submitted for assessment in this subject.
+
+   (2) I also certify that I have not provided a copy of this work
+   in either softcopy or hardcopy or any other form to any other
+   student, and nor will I do so until after the marks are released.
+   I understand that providing my work to other students, regardless
+   of my intention or any undertakings made to me by that other
+   student, is also Academic Misconduct.
+
+   (3) I declare that I have used generative AI tools in connection
+   with the following aspects of the assignment (please select the
+   option that applies best in your case and delete the other three
+   choices):
+
+   -- I prompted an AI system with the full assignment specification
+   and example input/output files, and requested a complete solution.
+   I then reviewed what I was provided with, made further alterations
+   to make sure it addressed the specification, including adding
+   suitable comments and this Authorship Declaration.
+
+   -- I developed the program structure for myself, and then used
+   a sequence of prompts to an AI system to write parts of my design
+   as individual functions (including, for example, via GitHub
+   copilot), but without including the assignment specification in
+   any of the prompts.
+
+   -- I developed my own complete solution to the problem, and then
+   used AI to help debug individual functions, find errors, and
+   provide other guidance in regard to structure and so on.
+
+   -- I made zero use of AI, and the program is wholly the result
+   my own knowledge and development.
+
+   (4) The AI system(s) that I used are:
+
+   [Please type a list of the AI systems that you used while doing
+   this assignment, or "None"]
+
+   (5) I understand that submitting for assessment work developed
+   in collaboration with other people constitutes Academic Misconduct,
+   and may be penalized by mark deductions, or by other penalties
+   determined via the University of Melbourne Academic Honesty
+   Policy, as described at https://academicintegrity.unimelb.edu.au.
+
+   Signed by: [Enter your full name and student number here before submission]
+   Dated:     [Enter the date that you "signed" the declaration]
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/**********************************************************************/
+
+/* put all your constants here */
+
+/**********************************************************************/
+
+/* put all your typedefs and structs here */
+
+/**********************************************************************/
+
+/* put all your function prototypes here */
+
+/**********************************************************************/
+
+int
+main(int argc, char *argv[]) {
+	
+	/* you have to write the body of the main function, but don't
+	   make it too long, it should control the traffic flow and use
+	   functions to do the actual work
+	*/
+
+	/* all done, time for a nap */
+	return EXIT_SUCCESS;
+}
+
+/**********************************************************************/
+
+/* add all your functions here, one function per major (or minor!)
+   task
+*/
+
